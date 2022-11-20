0c9f68186dddd547e3ce9db6daa90c3eb76336e6
Alhusaine NEMER <al@testndev.com>
2022-11-20 11:50:08 +0100

    add a report attachement helper

---
+++ test/helper/ReportAttachments.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+export default class ReportAttachments {
+
+  static addComparisonElements(testInfo, expectedList: any, parsingResult: any) {
+    const contentType = 'application/json';
+    const stringifiedExpectedJson = JSON.stringify(expectedList, null, 2);
+    const stringifiedResult = JSON.stringify(parsingResult, null, 2);
+    testInfo.attach('2-expected-result.json', { body: stringifiedExpectedJson, contentType });
+    testInfo.attach('3-obtained-result.json', { body: stringifiedResult, contentType });
+  }
+
+  static addInputFile(testInfo, datasetFilePath: string, encoding: BufferEncoding = 'utf-8') {
+    testInfo.attach(datasetFilePath, { body: fs.readFileSync(datasetFilePath, { encoding }), contentType: 'text/plain' });
+  }
+
+  static addInputText(testInfo, textualInput: string, encoding: BufferEncoding = 'utf-8') {
+    testInfo.attach('textual input', { body: textualInput, encoding: encoding, contentType: 'text/plain' });
+  }
+}
