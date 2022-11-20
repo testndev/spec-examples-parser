80ea4a562d5847a5d7873e2680ede9a35b42f1dc
Alhusaine NEMER <al@testndev.com>
2022-11-20 23:55:07 +0100

    add code coverage with istanbuljs

--- dist/SpecExamplesParser.d.ts
+++ dist/SpecExamplesParser.d.ts
@@ -1,4 +1,8 @@
 /// <reference types="node" />
+declare type GherkinParsingOptions = {
+    position?: number;
+    encoding?: BufferEncoding;
+};
 export declare class SpecExamplesParser {
     /**
      * Convert a text listing Examples, as in Gherkin format (Examples block in "Scenario Outline")
@@ -8,5 +12,11 @@ export declare class SpecExamplesParser {
     static fromGherkinFormatTable(examplesText: string): any;
     static fromCsv(filePath: string, encoding?: BufferEncoding): any;
     static fromTsv(filePath: string, encoding?: BufferEncoding): any;
+    static fromGherkinFeatureFile(filePath: string, options?: GherkinParsingOptions): never[];
+    private static positionsOfExamplesKeywords;
+    private static numberOfExamplesKeywords;
     static fromExcel(filePath: string): void;
+    private static isFileEmpty;
+    private static isFeatureFile;
 }
+export {};
--- dist/SpecExamplesParser.js
+++ dist/SpecExamplesParser.js
@@ -33,10 +33,92 @@ var SpecExamplesParser = /** @class */ (function () {
         });
         return dataTable;
     };
+    SpecExamplesParser.fromGherkinFeatureFile = function (filePath, options) {
+        var encoding = (options === null || options === void 0 ? void 0 : options.encoding) || 'utf-8';
+        var neededTableOrder = (options === null || options === void 0 ? void 0 : options.position) || 1;
+        var fullText = fs.readFileSync(filePath, { encoding: encoding });
+        var dataTable = [];
+        if (this.isFileEmpty(fullText)) {
+            throw new Error("\"".concat(filePath, "\" file is empty"));
+        }
+        else {
+            if (!this.isFeatureFile(fullText)) {
+                throw new Error("\"".concat(filePath, "\" file is not a \"feature\" file"));
+            }
+            else {
+                var numberOfExamplesKeywords = this.numberOfExamplesKeywords(fullText);
+                if (numberOfExamplesKeywords > 0) {
+                    if (numberOfExamplesKeywords >= neededTableOrder) {
+                        var indexesOf = SpecExamplesParser.positionsOfExamplesKeywords(fullText);
+                        var startSearchingAtLine = indexesOf[neededTableOrder - 1];
+                        var examplesTableText = firstExamplesTableText(fullText, startSearchingAtLine);
+                        dataTable = (0, sync_1.parse)(examplesTableText, {
+                            columns: true,
+                            skip_empty_lines: true,
+                            delimiter: '|',
+                            relax_quotes: true,
+                            trim: true
+                        });
+                    }
+                    else {
+                        throw new Error("\"".concat(filePath, "\" Feature file contains only ").concat(numberOfExamplesKeywords, " \"Scenario Outline\", asked for ").concat(neededTableOrder, "st"));
+                    }
+                }
+                else {
+                    throw new Error("\"".concat(filePath, "\" Feature file doesn't contain any \"Scenario Outline\""));
+                }
+            }
+        }
+        return dataTable;
+    };
+    SpecExamplesParser.positionsOfExamplesKeywords = function (fullText) {
+        return fullText.split('\n').map(function (x, i) { return isExamplesKeyword(x) ? i : -1; }).filter(function (i) { return i >= 0; });
+    };
+    SpecExamplesParser.numberOfExamplesKeywords = function (fullText) {
+        return fullText.split('\n').filter(isExamplesKeyword).length;
+    };
     SpecExamplesParser.fromExcel = function (filePath) {
         throw new Error("Not implemented yet");
+    };
+    SpecExamplesParser.isFileEmpty = function (text) {
+        return text === undefined || text.trim() === '';
+    };
+    SpecExamplesParser.isFeatureFile = function (text) {
+        return text.toLowerCase().includes('feature:');
     };
     return SpecExamplesParser;
 }());
 exports.SpecExamplesParser = SpecExamplesParser;
+function firstExamplesTableText(fullText, examplesKeywordLineIndex) {
+    var lines = fullText.split('\n');
+    var followingLines = lines.slice(examplesKeywordLineIndex + 1);
+    var examplesTableLines = [];
+    followingLines.every(function (line) {
+        var isAnExempleLine = line.trim().startsWith('|');
+        if (isAnExempleLine) {
+            examplesTableLines.push(line);
+        }
+        return isAnExempleLine;
+    });
+    return cleanExamplesTable(examplesTableLines).join('\n');
+}
+function isExamplesKeyword(line) {
+    return line.includes('Examples:');
+}
+function cleanExamplesTable(examplesTableLines) {
+    if (examplesTableLines && examplesTableLines.length === 0)
+        return [];
+    var header = examplesTableLines[0];
+    if (header.trim().startsWith('|')) {
+        return examplesTableLines
+            .map(function (line) {
+            var trim = line.trim();
+            //remove '|' at begin and end
+            return trim.substring(1, trim.length - 1);
+        });
+    }
+    else {
+        return examplesTableLines.map(function (line) { return line.trim(); });
+    }
+}
 //# sourceMappingURL=SpecExamplesParser.js.map
