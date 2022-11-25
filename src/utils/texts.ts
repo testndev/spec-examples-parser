db3f79f5c2b3ddfc8780915ddf0fcdcd60b251f1
Alhusaine NEMER <al@testndev.com>
2022-11-25 15:21:06 +0100

    delete empty lines in convertToCleanTsvFormat

--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -1,17 +1,23 @@
 function convertToCleanTsvFormat(examplesTableLines: string[]): string {
-  const header = examplesTableLines[0];
-  if (header.trim().startsWith('|')) {
-    return examplesTableLines
-      .map((line) => {
-        const trim = line.trim();
-        //remove '|' at begin and end
-        return trim.substring(1, trim.length - 1);
-      })
-      .join('\n');
-  } else {
-    return examplesTableLines
-      .map(line => line.trim())
-      .join('\n');
+  const tableLines = examplesTableLines.filter(line => !isTextBlankOrEmpty(line));
+  if (tableLines && tableLines.length > 1) {
+    const header = tableLines[0];
+    if (header.trim().startsWith('|')) {
+      return tableLines
+        .map((line) => {
+          const trim = line.trim();
+          //remove '|' at begin and end
+          return trim.substring(1, trim.length - 1);
+        })
+        .join('\n');
+    } else {
+      return tableLines
+        .map(line => line.trim())
+        .join('\n');
+    }
+  }
+  else {
+    return '';
   }
 }
 
