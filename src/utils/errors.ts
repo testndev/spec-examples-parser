8deabbd6a654392a934f0e0c98bb8c9bec438030
Alhusaine NEMER <al@testndev.com>
2022-12-05 22:51:50 +0100

    new throwable errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -13,8 +13,24 @@ function throwNotFeatureFileError(filePath: string) {
   throw new Error(`"${filePath}" file is not a "feature" file`);
 }
 
+function throwJsonNotRepresentingListError(filePath: string) {
+  throw new Error(`"${filePath}" JSON file contains an object, not a list of objects`);
+}
+
+function throwNotJsonParseableFileError(filePath: string) {
+  throw new Error(`"${filePath}" file is not a JSON-parseable file (error at first character)`);
+}
+
+function throwFailedToParseJsonFileError(filePath: string, originalError: Error) {
+  throw new Error(`Parsing "${filePath}" JSON file failed. ${originalError.message}`);
+}
+
+
 export {
   throwEmptyFileError,
   throwNotFeatureFileError,
-  throwNoEnoughScenarioOutlineInFileError
+  throwNoEnoughScenarioOutlineInFileError,
+  throwNotJsonParseableFileError,
+  throwFailedToParseJsonFileError,
+  throwJsonNotRepresentingListError
 }
