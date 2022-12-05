c8c46788044a864d198265b3f39b5765f9dc2b32
Alhusaine NEMER <al@testndev.com>
2022-12-05 22:53:04 +0100

    new example in list

--- test/expectedValues.ts
+++ test/expectedValues.ts
@@ -1,4 +1,4 @@
-export const greetingsWithSpaces = [
+const greetingsWithSpaces = [
   { name: 'Sébastien', language: 'french', greetingWord: 'Bonjour', expectedPhrase: 'Bonjour Sébastien !' },
   { name: 'édouard', language: 'english', greetingWord: 'Hello', expectedPhrase: 'Hello Édouard!' },
   { name: 'ROSA', language: 'spanish', greetingWord: 'Hola', expectedPhrase: '¡Hola Rosa!' },
@@ -6,24 +6,40 @@ export const greetingsWithSpaces = [
   { name: 'Samuel', language: 'danish', greetingWord: 'Hej', expectedPhrase: 'Hej Samuel!' },
 ];
 
-export const eatingCucumberLeft = [
+const eatingCucumberLeft = [
   { start: '12', eat: '5', left: '7' },
   { start: '20', eat: '5', left: '15' },
 ];
 
-export const eatingApplesLeft = [
+const eatingApplesLeft = [
   { start: '10', eat: '4', left: '6' },
 ];
 
 
-export const simpleAbcValues = [
+const simpleAbcValues = [
   { letter: 'A', order: '1' },
   { letter: 'B', order: '2' },
   { letter: 'Z', order: '26' },
 ];
 
-export const firstValueIsEmpty = [
+const abzWithOrdersAsNumber = [
+  { letter: 'A', order: 1 },
+  { letter: 'B', order: 2 },
+  { letter: 'Z', order: 26 },
+];
+
+const firstValueIsEmpty = [
   { name: '', language: 'english', greetingWord: 'Hello', expectedPhrase: 'Hello!' },
 ];
 
-export const emptyList = [];
+const emptyList = [];
+
+export {
+  emptyList,
+  firstValueIsEmpty,
+  simpleAbcValues,
+  abzWithOrdersAsNumber,
+  greetingsWithSpaces,
+  eatingApplesLeft,
+  eatingCucumberLeft
+}
