313efa8029ed2606ff59d1982616e06a9c7998ab
Alhusaine NEMER <al@testndev.com>
2023-01-29 08:44:14 +0100

    more unit tests

--- test/MagicMethod.test.ts
+++ test/MagicMethod.test.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 import { SpecExamplesParser as examples } from '../src/SpecExamplesParser';
 import ReportAttachments from './helper/ReportAttachments';
 import { examplesListOfGherkinExamples } from "./examplesListOfGherkinExamples";
+import { greetingsWithSpaces, simpleAbcValues, eatingCucumberLeft, abzWithOrdersAsNumber, eatingApplesLeft } from './expectedValues';
+import { thenResultEqualsExpectedArray } from './helper/steps';
 
 
 
@@ -30,7 +32,63 @@ test.describe('Parsing textual Gherkin Example with magic method', () => {
     });
   });
 
+});
 
+
+test.describe('Parsing files with magic method', () => {
+
+  [
+    {
+      testTitle: 'CSV with simple values',
+      inputFilePath: './test/specexamples/csv/simple-ABC-values.csv',
+      expectedList: simpleAbcValues,
+    },
+    {
+      testTitle: 'CSV with quotes',
+      inputFilePath: './test/specexamples/csv/greetings-with-quotes.csv',
+      expectedList: greetingsWithSpaces,
+    },
+    {
+      testTitle: 'TSV with quotes',
+      inputFilePath: './test/specexamples/tsv/greetings-with-quotes.tsv',
+      expectedList: greetingsWithSpaces,
+    },
+    {
+      testTitle: 'Gherkin file, with by default first examples table',
+      inputFilePath: './test/specexamples/gherkin/two-scenario-outlines.feature',
+      expectedList: eatingCucumberLeft,
+    },
+    {
+      testTitle: 'Gherkin file with .spec extension',
+      inputFilePath: './test/specexamples/gherkin/single-scenario-outline.spec',
+      expectedList: eatingApplesLeft,
+    },
+    {
+      testTitle: 'JSON, with numbers as numerical',
+      inputFilePath: './test/specexamples/json/abzWithOrdersAsNumber.json',
+      expectedList: abzWithOrdersAsNumber,
+    },
+    {
+      testTitle: 'non-empty TXT file',
+      inputFilePath: './test/specexamples/unknownFileType.txt',
+      expectedList: [],
+    },
+  ]
+    .forEach(({ testTitle, inputFilePath, expectedList }) => {
+      test(testTitle, async ({ }, testInfo) => {
+        const result = await test.step(`when SpecExamplesParser is asked to parse "${inputFilePath}" file`, async () => {
+          ReportAttachments.addInputFile(testInfo, inputFilePath);
+          return examples.from(inputFilePath);
+        });
+        await thenResultEqualsExpectedArray(testInfo, expectedList, result);
+      });
+    });
+
+
+    test('Parsing blank text with magic method', async ({ }, testInfo) => {
+      const result = examples.from('   ');
+      await thenResultEqualsExpectedArray(testInfo, [], result);
+    });
 });
 
 
